d8c98ef9d7f58cfc38fd827ce176c9cc6f83cc06
Zahidul-Turja <zahidulturja@gmail.com>
2025-03-19 11:55:40 +0600

    Refactor Dockerfile and docker-compose.yml for cleaner setup; update settings.py for authentication backend

--- ghorkhoje/ghorkhoje/settings.py
+++ ghorkhoje/ghorkhoje/settings.py
@@ -98,12 +98,10 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+
 AUTH_USER_MODEL = "user.User"
-# TODO: Implement an Authentication Backend
-# AUTHENTICATION_BACKENDS = {
-#     "user.backends.EmailOrPasswordBackend",
-#     "django.contrib.auth.backends.ModelBackend",
-# }
+
 
 REST_USE_JWT = True
 REST_FRAMEWORK = {
