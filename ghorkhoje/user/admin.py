288d6b3e56c457e5558f82e996b0542722e8bfe2
Zahidul-Turja <zahidulturja@gmail.com>
2025-04-15 13:41:26 +0600

    Enhance LandlordAdmin to include updated_at field and applicant_info method for detailed applicant display

--- ghorkhoje/user/admin.py
+++ ghorkhoje/user/admin.py
@@ -1,3 +1,4 @@
+from django.utils.html import format_html
 from django.contrib import admin
 
 from user.models import *
@@ -21,11 +22,54 @@ class UserAdmin(admin.ModelAdmin):
 
 
 class LandlordAdmin(admin.ModelAdmin):
-    list_display = ("user", "status", "application_date")
+    list_display = ("user", "status", "application_date", "updated_at")
     list_filter = ("status",)
     search_fields = ("user__full_name", "user__email")
     ordering = ("-application_date",)
     list_per_page = 20
+
+    readonly_fields = ("applicant_info", "application_date", "updated_at")
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "user",
+                    "status",
+                    "rejection_reason",
+                    "application_date",
+                    "updated_at",
+                )
+            },
+        ),
+        (
+            "Applicant Details",
+            {
+                "fields": ("applicant_info",),
+            },
+        ),
+    )
+
+    def applicant_info(self, obj):
+        user = obj.user
+        return format_html(
+            f"""
+            <div style="line-height: 1.6;">
+                <strong>Name:</strong> {user.full_name}<br>
+                <strong>Email:</strong> {user.email}<br>
+                <strong>Phone:</strong> {user.phone}<br>
+                <strong>Gender:</strong> {user.gender}<br>
+                <strong>Date of Birth:</strong> {user.date_of_birth}<br>
+                <strong>Profession:</strong> {user.profession}<br>
+                <strong>NID:</strong> {user.nid}<br>
+                <strong>User Type:</strong> {user.user_type}<br>
+                {"<br><img src='{}' width='100' style='border-radius: 8px;' />".format(user.profile_image.url) if user.profile_image else ""}
+            </div>
+            """
+        )
+
+    applicant_info.short_description = "Applicant Info"
 
 
 admin.site.register(User, UserAdmin)
